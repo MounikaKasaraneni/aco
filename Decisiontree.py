cfb37ace768f55070f056a31b98d6da31090d21d
acoproject123 <37808394+acoproject123@users.noreply.github.com>
2018-04-13 17:27:03 -0500

    Update Decisiontree.py

--- Decisiontree.py
+++ Decisiontree.py
@@ -1,3 +1,4 @@
+#********Importing the pyspark statement
 from pyspark import SparkContext
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import DecisionTreeClassifier
@@ -7,16 +8,19 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from pyspark.ml.linalg import Vectors, VectorUDT
 from pyspark.mllib.evaluation import MultilabelMetrics
-
+#********* Intializing the spark context
 sc = SparkContext(appName="DecisionTreeClassifier")
+#*********** Reading the whole file and spliting the data by ,
 data = sc.textFile( "train_data.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=760).collect()
 label=sc.textFile( "train_labels.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=1).map(lambda line: float(line[0])).collect()
 test= sc.textFile( "test_data.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=760).collect()
 testlabel= sc.textFile("test_labels.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=1).map(lambda line: float(line[0])).collect()   
+#****************** Converting the data into RDD format
 data_rdd=sc.parallelize(data)
 test_rdd=sc.parallelize(test)
 testlabels_rdd= test_rdd.map(lambda seq:(float(seq[-1]),Vectors.dense((seq[:-2]))))
 labeled_points_rdd= data_rdd.map(lambda seq:(float(seq[-1]),Vectors.dense((seq[:-2]))))
+#******** creating the spark session to convert RDD into Dataframe
 spark = SparkSession(sc)
 hasattr(testlabels_rdd, "toDF")
 hasattr(labeled_points_rdd, "toDF")
@@ -27,23 +31,21 @@ test_labels = test_labels.selectExpr("_1 as label", "_2 as features")
 print(data.show(5))
 print(test_labels.show(5))
 
-# Split the data into training and test sets (30% held out for testing)
+# Assinging the training and testing models
 trainingData = data
 testingData = test_labels
 print("fitting data")
 labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
 featureIndexer =VectorIndexer(inputCol="features", outputCol="indexedFeatures").fit(data)
-# Train a DecisionTree model.
-
+# **************Train a DecisionTree model.
 dt = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
-
-# Chain indexers and tree in a Pipeline
+# ****************Chain indexers and tree in a Pipeline
 pipeline = Pipeline(stages=[labelIndexer, featureIndexer, dt])
 print("train")
-# Train model.  This also runs the indexers.
+# **************Train model.  This also runs the indexers.
 model = pipeline.fit(trainingData)
 print("testing")
-# Make predictions.
+# ***********Make predictions.
 predictions = model.transform(testingData)
 #predictionsIndexer = StringIndexer(inputCol="prediction", outputCol="prediction1").fit(predictions).transform(predictions)
 # Select example rows to display.
