f650da3d350eda592cb0c73c9bcdf02ae28c3606
acoproject123 <37808394+acoproject123@users.noreply.github.com>
2018-04-13 17:18:35 -0500

    Update Naive.py

--- Naive.py
+++ Naive.py
@@ -1,5 +1,5 @@
+#****** importing libraries that the pyspark need to run*******
 from pyspark import SparkContext
-import re # $example on$
 from pyspark.ml.classification import NaiveBayes
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.mllib.util import MLUtils
@@ -10,28 +10,25 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from pyspark.ml.linalg import Vectors, VectorUDT
 
+#**** Intializing the spark context*****
 sc = SparkContext(appName="PythonNaiveBayesExample")
-
-    # $example on$
-    # Load and parse the data file.
-
+# ******Reading the files and spliting them using comma******* 
 data = sc.textFile( "train_data.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=760).collect()
-    #print(np.array(data).tolist())#data=data.map(lambda line: line.split(",")).collect()
 label=sc.textFile( "train_labels.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=1).map(lambda line: float(line[0])).collect()
-    # Split data approximately into training (60%) and test (40%)
- #   training, test = data.randomSplit([0.6, 0.4])
-test= sc.textFile( "test_data.csv").map(lambda line: line.split(",")).collect()
-    
+test= sc.textFile( "test_data.csv").map(lambda line: line.split(",")).collect()    
 testlabel= sc.textFile("test_labels.csv").map(lambda line: line.split(",")).collect()   
+#********Converting the data into RDD********
 data_rdd=sc.parallelize(data)
 labels_rdd=sc.parallelize(label)
 labeled_points_rdd= data_rdd.map(lambda seq:(float(seq[-1]),Vectors.dense(seq[:-1])))
+#********** Converting the RDD data into DataFrame
 spark = SparkSession(sc)
 hasattr(labeled_points_rdd, "toDF")
 #labeled_points_rdd.toDF().show()
 data_labels=spark.createDataFrame(labeled_points_rdd)
 df = data_labels.selectExpr("_1 as label", "_2 as features")
 print(df.show(5))
+#******* Giving the model as Naive Bayes and modelType ="multinomial
 nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
 model = nb.fit(df)
     
