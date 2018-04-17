98c9516ec579b5526c234bd77fc72a9ab527c19b
Ruchishya <mail4ruchishya@gmail.com>
2018-04-16 21:56:58 -0500

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -1,45 +1,22 @@
-#********Importing the pyspark statement
 from pyspark import SparkContext
-from pyspark.ml import Pipeline
-from pyspark.ml.classification import Linearsvc
-from pyspark.ml.feature import StringIndexer, VectorIndexer
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
-from pyspark.ml.linalg import Vectors, VectorUDT
-from pyspark.mllib.evaluation import MultilabelMetrics
-#********* Intializing the spark context
-sc = SparkContext(appName="LinearSVC")
-#*********** Reading the whole file and spliting the data by ,
-data = sc.textFile( "train_data.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=760).collect()
-label=sc.textFile( "train_labels.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=1).map(lambda line: float(line[0])).collect()
-test= sc.textFile( "test_data.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=760).collect()
-testlabel= sc.textFile("test_labels.csv").map(lambda line: line.split(",")).filter(lambda line: len(line)<=1).map(lambda line: float(line[0])).collect()   
-#****************** Converting the data into RDD format
-data_rdd=sc.parallelize(data)
-test_rdd=sc.parallelize(test)
-testlabels_rdd= test_rdd.map(lambda seq:(float(seq[-1]),Vectors.dense((seq[:-2]))))
-labeled_points_rdd= data_rdd.map(lambda seq:(float(seq[-1]),Vectors.dense((seq[:-2]))))
-#******** creating the spark session to convert RDD into Dataframe
-spark = SparkSession(sc)
-hasattr(testlabels_rdd, "toDF")
-hasattr(labeled_points_rdd, "toDF")
-test_labels=spark.createDataFrame(testlabels_rdd)
-data_labels=spark.createDataFrame(labeled_points_rdd)
-data = data_labels.selectExpr("_1 as label", "_2 as features")
-test_labels = test_labels.selectExpr("_1 as label", "_2 as features")
-print(data.show(5))
-print(test_labels.show(5))
+from pyspark.mllib.classification import SVMWithSGD, SVMModel
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.util import MLUtils
+sc = SparkContext(appName="SVM")
 
-# Assinging the training and testing models
-# Load training data
-training = data
+# Split the data into training and test sets (30% held out for testing)
 
-lsvc = LinearSVC(maxIter=10, regParam=0.1)
+def parsePoint(line):
+    values = [float(x) for x in line.split(',')]
+    return LabeledPoint(values[0], values[1:])
 
-# Fit the model
-lsvcModel = lsvc.fit(training)
-evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
-# Select (prediction, true label) and compute test error
-accuracy = evaluator.evaluate(predictions)
-print("Accuracy = %g " % (accuracy))
+data = sc.textFile("train_data.csv")
+parsedData = data.map(parsePoint)
+testdata = sc.textFile("test_data.csv")
+testData1 = testdata.map(parsePoint)
+# Build the model
+model = SVMWithSGD.train(parsedData, iterations=100)
+predictions = model.predict(testData1.map(lambda x: x.features))
+labelsAndPredictions = testData1.map(lambda lp: lp.label).zip(predictions)
+testErr = labelsAndPredictions.filter(lambda lp: lp[0] == lp[1]).count() / float(testData1.count())
+print('Accuracy = %f '%testErr)
